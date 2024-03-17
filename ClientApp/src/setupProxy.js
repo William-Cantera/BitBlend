f2c09f05ceb84802e40dd6d5369679d3afe132ce
William Cantera <wfcantera@gmail.com>
2024-03-16 22:22:38 -0400

    update example prod url

--- ClientApp/src/setupProxy.js
+++ ClientApp/src/setupProxy.js
@@ -1,8 +1,10 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const { env } = require('process');
 
-const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:60367';
+const target = 'https://master.d12yk2hn6rz66e.amplifyapp.com/';
+
+//env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
+//  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:60367';
 
 const context = [
   "/weatherforecast",

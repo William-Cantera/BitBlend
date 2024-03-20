0173626eaf212eab8c5ff0e8759c131c77aca468
William Cantera <wfcantera@gmail.com>
2024-03-20 00:23:55 -0400

    final attempt, updating route in startup.cs

--- Startup.cs
+++ Startup.cs
@@ -17,16 +17,13 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-//app.UseStaticFiles();
-
 app.UseRouting();
-
-app.UseCors("AllowSpecificOrigin"); // Use CORS
+app.UseCors("AllowSpecificOrigin");
+app.UseStaticFiles();
 
 #pragma warning disable ASP0014 // Suggest using top level route registrations
 app.UseEndpoints(endpoints =>
@@ -34,19 +31,10 @@ app.UseEndpoints(endpoints =>
     endpoints.MapControllers();
     endpoints.MapControllerRoute(
         name: "api",
-        pattern: "api/{controller}/{action=Index}/{id?}"); // Define API route
+        pattern: "api/{controller}/{action}");
 
-    // For other routes, fall back to index.html
     endpoints.MapFallbackToFile("index.html");
 });
 #pragma warning restore ASP0014 // Suggest using top level route registrations
-
-// Remove the custom middleware that rewrites requests to "/index.html"
-// as it may interfere with API requests.
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller}/{action=Index}/{id?}");
-
 
 app.Run();
